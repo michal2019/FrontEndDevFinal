0a929db3b0153559068e947d61ec14dcd3d505fb
michal2019 <michal.rechler@gmail.com>
2019-06-26 16:05:48 +0300

    login form created, including login button (no logic)

--- app/login/loginCtrl.js
+++ app/login/loginCtrl.js
@@ -1,2 +1,12 @@
-app.controller("loginCtrl", function($scope) { 
-}); 
+
+app.controller("loginCtrl", function($scope, $location, $log ) {
+
+    $scope.invalidLogin = false;
+    $scope.email = "michal.rechler@gmail.com";
+    $scope.pwd = "12345";
+
+    $scope.login = function() {
+        
+    }
+
+})
